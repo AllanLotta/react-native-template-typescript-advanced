e626d97d079f1e71374eaf493eacf3bc6d79d91f
Allan Lotta <allanlotta.dev@gmail.com>
2020-10-21 13:34:35 -0300

    added react antive vector icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View, Text, SafeAreaView } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const App: React.FC = () => (
 	<SafeAreaView style={{ flex: 1 }}>
 		<Text>Advanced Template React Native</Text>
+		<Icon name="brain" size={30} color="blue" />
 	</SafeAreaView>
 );
 
